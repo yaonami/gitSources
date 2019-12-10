85309dc6b611afc9f714cc171b43fdf1714a4cae
yaonami <386319613@qq.com>
2019-12-10 21:02:10 +0800

    正式上线机器人和不同时段消息回复

--- itchat_demo/login.py
+++ itchat_demo/login.py
@@ -19,65 +19,8 @@ def print_time():
 
 
 @itchat.msg_register(TEXT)
-def sendWeather(msg):
-    type_info = msg['Text'].split('-')[0]
-    area = msg['Text'].split('-')[1]
-    res = ''
-    if type_info=='实况天气':
-        weather_info = weather.getWeatherNow(area)
-        print(weather_info)
-        info = '当前所在地区：'+weather_info['地区']+'\n'+'天气更新时间：'+weather_info['更新时间']+'\n'+'当前天气：'\
-               +weather_info['当前天气']+'\n'+'当前风向：'+weather_info['当前风向']+'\n'+'当前风力：'\
-               +weather_info['当前风力']+'级'+'\n'+'能见度：'+weather_info['能见度']+' KM'+'\n'+'当前温度：'\
-               +weather_info['当前温度']+'℃'+'\n'+'体感温度：'+weather_info['体感温度']+'℃'
-        res = info
-    elif type_info=='未来7天天气预报':
-        weather_info = weather.getWeatherForecast(area)
-        print(weather_info)
-        infos = ''
-        for w in weather_info:
-            info = '日期：'+w['日期']+'\n'+'白天天气：'+w['白天天气']+'\n'+'夜间天气：'+w['夜间天气']+'\n'+'风向：'\
-                   +w['风向']+'\n'+'风力：'+w['风力']+'级'+'\n'+'最低温度：'+w['最低温度']+'℃'+'\n'+'最高温度：'\
-                   +w['最高温度']+'℃'+'\n'+'能见度：'+w['能见度']+' KM'+'\n'+'--------'+'\n'
-            infos = infos + info
-        res = infos
-    elif type_info=='未来24小时天气预报':
-        weather_info = weather.getWeatherHourly(area)
-        print(weather_info)
-        infos = ''
-        for w in weather_info:
-            info = '时间：'+w['时间']+'\n'+'天气：'+w['天气']+'\n'+'风向：'+w['风向']+'\n'+'风力：'\
-                   +w['风力']+'级'+'\n'+'温度：'+w['温度']+'℃'+'\n'+'--------'+'\n'
-            infos = infos + info
-        res = infos
-    elif type_info == '生活指数':
-        weather_info = weather.getWeatherLifestyle(area)
-        infos = ''
-        for w in weather_info:
-            f,b = str(w)[1:len(str(w))-1].split(':')
-            info = f[1:len(f)-1] + '：'+b[2:len(b)-1]+'\n'+'========'+'\n'
-            infos = infos + info
-        res = infos
-    return res
-
-
-# @itchat.msg_register(TEXT)
-# def sendMsgByRobot(msg):
-#     global user_status
-#     if user_status[msg['FromUserName']]:
-#         if msg['Text']=='小叮咚再见':
-#             itchat.send('再见!', msg['FromUserName'])
-#             user_status[msg['FromUserName']] = False
-#         else:
-#             info = robot.get_response(msg['Text'])
-#             itchat.send(info, msg['FromUserName'])
-#     else:
-#         if msg['Text']=='小叮咚':
-#             itchat.send('在的呢!', msg['FromUserName'])
-#             user_status[msg['FromUserName']] = True
-
-@itchat.msg_register(TEXT)
-def sendMsgByRobot(msg):
+def sendMsgByQYKRobot(msg):
+    print(msg)
     global user_status
     if user_status[msg['FromUserName']]:
         if msg['Text']=='小叮咚再见':
@@ -92,30 +35,112 @@ def sendMsgByRobot(msg):
             user_status[msg['FromUserName']] = True
 
 
+# @itchat.msg_register(TEXT)
+# def sendWeather(msg):
+#     print(msg)
+#     type_info = msg['Text'].split('-')[0]
+#     area = msg['Text'].split('-')[1]
+#     res = ''
+#     if type_info=='实况天气':
+#         weather_info = weather.getWeatherNow(area)
+#         print(weather_info)
+#         info = '当前所在地区：'+weather_info['地区']+'\n'+'天气更新时间：'+weather_info['更新时间']+'\n'+'当前天气：'\
+#                +weather_info['当前天气']+'\n'+'当前风向：'+weather_info['当前风向']+'\n'+'当前风力：'\
+#                +weather_info['当前风力']+'级'+'\n'+'能见度：'+weather_info['能见度']+' KM'+'\n'+'当前温度：'\
+#                +weather_info['当前温度']+'℃'+'\n'+'体感温度：'+weather_info['体感温度']+'℃'
+#         res = info
+#     elif type_info=='未来7天天气预报':
+#         weather_info = weather.getWeatherForecast(area)
+#         print(weather_info)
+#         infos = ''
+#         for w in weather_info:
+#             info = '日期：'+w['日期']+'\n'+'白天天气：'+w['白天天气']+'\n'+'夜间天气：'+w['夜间天气']+'\n'+'风向：'\
+#                    +w['风向']+'\n'+'风力：'+w['风力']+'级'+'\n'+'最低温度：'+w['最低温度']+'℃'+'\n'+'最高温度：'\
+#                    +w['最高温度']+'℃'+'\n'+'能见度：'+w['能见度']+' KM'+'\n'+'--------'+'\n'
+#             infos = infos + info
+#         res = infos
+#     elif type_info=='未来24小时天气预报':
+#         weather_info = weather.getWeatherHourly(area)
+#         print(weather_info)
+#         infos = ''
+#         for w in weather_info:
+#             info = '时间：'+w['时间']+'\n'+'天气：'+w['天气']+'\n'+'风向：'+w['风向']+'\n'+'风力：'\
+#                    +w['风力']+'级'+'\n'+'温度：'+w['温度']+'℃'+'\n'+'--------'+'\n'
+#             infos = infos + info
+#         res = infos
+#     elif type_info == '生活小贴士':
+#         weather_info = weather.getWeatherLifestyle(area)
+#         infos = ''
+#         for w in weather_info:
+#             f,b = str(w)[1:len(str(w))-1].split(':')
+#             info = f[1:len(f)-1] + '：'+b[2:len(b)-1]+'\n'+'========'+'\n'
+#             infos = infos + info
+#         res = infos
+#     return res
+#
+#
+# @itchat.msg_register(TEXT)
+# def sendMsgByTLRobot(msg):
+#     global user_status
+#     if user_status[msg['FromUserName']]:
+#         if msg['Text']=='小叮咚再见':
+#             itchat.send('再见!', msg['FromUserName'])
+#             user_status[msg['FromUserName']] = False
+#         else:
+#             info = robot.get_response(msg['Text'])
+#             itchat.send(info, msg['FromUserName'])
+#     else:
+#         if msg['Text']=='小叮咚':
+#             itchat.send('在的呢!', msg['FromUserName'])
+#             user_status[msg['FromUserName']] = True
+
+
 if __name__ == "__main__":
     global user_status
     login()
     user_status = friend.createUserStatusDict()
-    # friend.get_friend('@cd6ec55721dbed471f992ad8de4b5877c06bb7243814b62fff0bb26a656a5d10')
+    sentence_remarkNames = ['A大树','D肖健伟','XC罗','X度小囡','X李涛','X十一','X云熙','XL先生']
+    # sentence_remarkNames = ['A大树']
+    sendMS.sendMsgTips(sentence_remarkNames)
+    # sendMS.sendEvening(sentence_remarkNames)
+    # sendMS.sendMorning(sentence_remarkNames)
+    # sendMS.sendTips(sentence_remarkNames)
+    # sendMS.sendWeatherTips(sentence_remarkNames)
+    # sendMS.sendSentence(sentence_remarkNames)
+    # itchat.send_file('1.mp3', 'filehelper')
+    # sendMS.sendPicture('pic7.gif')
+    # sendMS.sendPictureByNickName('一人一歌一世界','pic7.gif')
+    # friend.get_friend('文件传输助手')
     # print(user_status)
     # print(tmp)
     # scheduler = BlockingScheduler()
-    # scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler()
     # # scheduler.add_job(print_time, 'date', run_date='2019-12-7 14:35:00')
-    # scheduler.add_job(login, 'cron', hour='6', minute='30')
+    # scheduler.add_job(sendMsgByQYKRobot, trigger='cron', hour='20', minute='18')
+    # scheduler.add_job(sendWeather, trigger='cron', hour='20', minute='20')
+    scheduler.add_job(login, 'cron', hour='22', minute='00')
+    scheduler.add_job(sendMS.sendTips, args=[sentence_remarkNames,], trigger='cron', hour='22', minute='01')
+    # scheduler.add_job(sendMS.sendTips, args=[sentence_remarkNames,], trigger='cron', hour='20', minute='36')
+    scheduler.add_job(sendMS.sendEvening, args=[sentence_remarkNames,], trigger='cron', hour='22', minute='30')
+    # scheduler.add_job(sendMS.sendEvening, args=[sentence_remarkNames,], trigger='cron', hour='20', minute='37')
     # # scheduler.add_job(print_time, 'interval', seconds=10)
     # scheduler.add_job(sendMS.sendMsg, trigger='cron', hour='15', minute='03')
     # scheduler.add_job(sendMS.sendMsg, args=['起床啦，新的一天从微笑开始！',], trigger='cron', hour='15', minute='11')
-    # scheduler.start()
-    itchat.run()
+    scheduler.add_job(sendMS.sendMorning, args=[sentence_remarkNames, ], trigger='cron', hour='7', minute='0')
+    # scheduler.add_job(sendMS.sendMorning, args=[sentence_remarkNames, ], trigger='cron', hour='20', minute='38')
+    scheduler.add_job(sendMS.sendSentence, args=[sentence_remarkNames,], trigger='cron', hour='7', minute='30')
+    # scheduler.add_job(sendMS.sendSentence, args=[sentence_remarkNames,], trigger='cron', hour='20', minute='39')
+    # scheduler.add_job(sendMS.sendWeatherTips, args=[sentence_remarkNames,], trigger='cron', hour='8', minute='0')
+    scheduler.start()
     # print_time()
     # tmp = weather.getWeatherForecast('北京')
     # tmp = weather.getWeatherHourly('北京')
     # tmp = weather.getWeatherLifestyle('北京')
     # tmp = weather.getWeatherNow('南昌')
     # print(tmp)
-    # while True:
-    #     pass
+    itchat.run()
+    while True:
+        pass
     # info  = robot.get_response('123')
     # print(info)
 
--- itchat_demo/sendMS.py
+++ itchat_demo/sendMS.py
@@ -1,9 +1,15 @@
 import itchat
+import time
+
+import message
+import weather
+import robot
+import friend
 
 
 # 发送消息给自己
-def sendMsg(msg):
-    itchat.send(msg, '@b5bc0ef02627f80b23f63efdf6b0fd22764f9533945900cd51726a2767a234d1')
+# def sendMsg(msg):
+#     itchat.send(msg, '@b5bc0ef02627f80b23f63efdf6b0fd22764f9533945900cd51726a2767a234d1')
 
 
 # 通过昵称发送消息
@@ -11,4 +17,71 @@ def sendMsgByNickName(nickName,msg):
     info = itchat.search_friends(nickName)[0]
     itchat.send(msg, info['UserName'])
 
+# 发送图片给自己
+# def sendPicture(picture):
+#     itchat.send_image(picture, '@b5bc0ef02627f80b23f63efdf6b0fd22764f9533945900cd51726a2767a234d1')
 
+
+# 通过昵称发送图片
+def sendPictureByNickName(nickName,picture):
+    info = itchat.search_friends(nickName)[0]
+    print(info)
+    itchat.send_image(picture, info['UserName'])
+
+
+# 发送每日推荐句子
+def sendSentence(remarkNames):
+    content,note,voicePath = message.getSentence()
+    msg = '今天分享的句子：' + '\n' + content + '\n' + note +'\n'+'下面是音频文件'
+    for remarkName in remarkNames:
+        info = itchat.search_friends(remarkName)[0]
+        itchat.send_msg(msg, info['UserName'])
+        itchat.send_file(voicePath, info['UserName'])
+
+
+# 发送提示
+def sendTips(remarkNames):
+    msg = '小叮咚上线啦' + '\n' + '每天22:00-8:00' + '\n' + '开启聊天请回复"小叮咚"' + '\n' + '结束聊天请回复"小叮咚再见"'
+    for remarkName in remarkNames:
+        info = itchat.search_friends(remarkName)[0]
+        itchat.send_msg(msg, info['UserName'])
+
+
+# 发送早上好
+def sendMorning(remarkNames):
+    msg = '新的一天从微笑开始,早安！'
+    today = time.strftime("%Y%m%d")
+    picture = 'picture/' + today + '-morning.gif'
+    # print(picture)
+    for remarkName in remarkNames:
+        info = itchat.search_friends(remarkName)[0]
+        itchat.send_msg(msg, info['UserName'])
+        itchat.send_image(picture, info['UserName'])
+
+
+# 发送天气提示
+def sendWeatherTips(remarkNames):
+    msg = '天气小助手上线啦' + '\n' + '回复数字后面内容：' + '\n' + '1.实况天气-所在城区' + '\n' \
+          + '2.未来7天天气预报-所在城区' + '\n' + '3.未来24小时天气预报-所在城区' + '\n' + '4.生活小贴士-所在城区' + '\n'
+    for remarkName in remarkNames:
+        info = itchat.search_friends(remarkName)[0]
+        itchat.send_msg(msg, info['UserName'])
+
+
+def sendEvening(remarkNames):
+    msg = '让夜风带走一天的疲惫，让月光驱走一天的烦恼，让星星点亮一天的好心情，祝你轻松入眠，美梦连篇，晚安。'
+    today = time.strftime("%Y%m%d")
+    picture = 'picture/' + today + '-evening.gif'
+    for remarkName in remarkNames:
+        info = itchat.search_friends(remarkName)[0]
+        itchat.send_msg(msg, info['UserName'])
+        itchat.send_image(picture, info['UserName'])
+
+
+# 程序启动提示
+def sendMsgTips(remarkNames):
+    msg = '做个小测试，接下来的几天可能会不定时收到来自本账号的消息，如果觉得被打扰，请微信回复本账号，' \
+          '如果有任何建议或意见，也可以微信回复，谢谢您'
+    for remarkName in remarkNames:
+        info = itchat.search_friends(remarkName)[0]
+        itchat.send_msg(msg, info['UserName'])
